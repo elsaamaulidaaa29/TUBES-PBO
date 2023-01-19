64b1e751068e475bfd2c14eca0918c20349f2e98
elsaaa29 <elsamaullida29@gmail.com>
2023-01-19 22:56:10 +0700

    Tubes PBO

---
+++ src/main/java/praktikum1/crudtugas/controller/TugasController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum1.crudtugas.controller;
+
+import praktikum1.crudtugas.interfc.TugasInterface;
+import praktikum1.crudtugas.models.Tugas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class TugasController implements TugasInterface {
+//    public Tugas insert(){
+//        
+//    }
+}
